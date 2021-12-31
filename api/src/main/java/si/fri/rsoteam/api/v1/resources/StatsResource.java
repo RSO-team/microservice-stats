34e9baa72b03633fb2c5417360df16c88fe21255
creativ10 <mv5195@student.uni-lj.si>
2021-12-31 05:21:34 +0100

    added annotations for open-api. Fix dependencies

--- api/src/main/java/si/fri/rsoteam/api/v1/resources/StatsResource.java
+++ api/src/main/java/si/fri/rsoteam/api/v1/resources/StatsResource.java
@@ -34,29 +34,71 @@ public class StatsResource {
     protected UriInfo uriInfo;
 
     @GET
+    @Operation(summary = "Returns list of all stats.", description = "Returns list of all stats..")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully returned all stats.",
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = StatsDto.class)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+
+            )
+    })
     public Response getStats() {
         return Response.ok(statsBean.getAllStats()).build();
     }
 
     @GET
     @Path("/{objectId}")
+    @Operation(summary = "Gets specific stat by id.", description = "Returns specific stat.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully returned specific stats.",
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = StatsDto.class))
+            )
+    })
     public Response getStatsById(@PathParam("objectId") Integer id) {
         return Response.ok(statsBean.getStats(id)).build();
     }
 
     @POST
+    @Operation(summary = "Create new stat.", description = "Create new stat.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully created new stat.",
+                    responseCode = "201",
+                    content = @Content(schema = @Schema(implementation = StatsDto.class))
+            )
+    })
     public Response createStats(StatsDto statsDto) {
         return Response.status(201).entity(statsBean.createStats(statsDto)).build();
     }
 
     @PUT
     @Path("{objectId}")
+    @Operation(summary = "Update specific stat.", description = "Update specific stat by id.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully updated stat.",
+                    responseCode = "201",
+                    content = @Content(schema = @Schema(implementation = StatsDto.class))
+            )
+    })
     public Response updateStats(@PathParam("objectId") Integer id, StatsDto eventDto) {
         return Response.status(201).entity(statsBean.updateStats(eventDto, id)).build();
     }
 
     @DELETE
     @Path("{objectId}")
+    @Operation(summary = "Delete specific stat.", description = "Delete specific stat by id.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully deleted stat.",
+                    responseCode = "201",
+                    content = @Content(schema = @Schema(implementation = StatsDto.class))
+            )
+    })
     public Response deleteEvent(@PathParam("objectId") Integer id) {
         statsBean.deleteStats(id);
         return Response.status(204).build();

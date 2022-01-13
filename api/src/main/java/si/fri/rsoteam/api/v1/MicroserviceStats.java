ea0deaef8d70a098c2b5814e08692cf36a2b1bbc
Luka Galjot <luka.galjot@gmail.com>
2022-01-13 22:49:49 +0100

    hc openapi url

--- api/src/main/java/si/fri/rsoteam/api/v1/MicroserviceStats.java
+++ api/src/main/java/si/fri/rsoteam/api/v1/MicroserviceStats.java
@@ -18,7 +18,7 @@ import javax.ws.rs.core.Application;
                 contact = @Contact(),
                 license = @License(name = "none")
         ),
-        servers = @Server(url = "http://localhost:8086"),
+        servers = @Server(url = "http://52.226.192.49/stats"),
         security = @SecurityRequirement(name = "none"))
 @RegisterService
 @ApplicationPath("/v1")
